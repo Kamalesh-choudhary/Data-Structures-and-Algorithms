c7418755e03ac187500f1f26d02045b7236238bf
Kamalesh-choudhary <157907909+Kamalesh-choudhary@users.noreply.github.com>
2026-08-16 15:30:40 +0530

    Add: unique-paths-ii - submission-1

---
+++ Data Structures & Algorithms/unique-paths-ii/submission-1.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int fn(int i,int j,vector<vector<int>>& grid,vector<vector<int>>& dp){
+        if(i<0 || j<0 || grid[i][j] == 1){
+            return 0;
+        }
+        if(i==0 && j==0){
+            return 1;
+
+        }
+        if(dp[i][j] != -1){
+            return dp[i][j];
+        }
+        return dp[i][j] = fn(i-1,j,grid,dp) + fn(i,j-1,grid,dp);
+    }
+    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+        int m = obstacleGrid.size();
+        int n = obstacleGrid[0].size();
+        vector<vector<int>> dp(m,vector<int>(n,-1));
+        return fn(m-1,n-1,obstacleGrid,dp);
+    }
+};
