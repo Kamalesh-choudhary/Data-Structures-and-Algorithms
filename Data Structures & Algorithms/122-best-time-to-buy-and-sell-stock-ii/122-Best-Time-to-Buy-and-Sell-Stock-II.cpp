926e0cee73b5bf82ef8370f41b515ea1edadcde5
Kamalesh-choudhary <157907909+Kamalesh-choudhary@users.noreply.github.com>
2026-08-23 18:01:34 +0530

    [122] [Time Beats: 6.12%] [Memory Beats: 8.26%] - LeetPush

---
+++ Data Structures & Algorithms/122-best-time-to-buy-and-sell-stock-ii/122-Best-Time-to-Buy-and-Sell-Stock-II.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int fn(int i,bool buy,vector<int>& prices,vector<vector<int>>& dp){
+        if(i==prices.size()) return 0;
+        if(dp[i][buy] != -1) return  dp[i][buy];
+        long long profit = 0;
+        if(buy){
+            profit = max(-prices[i] + fn(i+1,!buy,prices,dp),0+fn(i+1,buy,prices,dp));
+        }
+        else{
+            profit = max(prices[i]+fn(i+1,!buy,prices,dp),0+fn(i+1,buy,prices,dp));
+        }
+        return dp[i][buy] = profit;
+    }
+    int maxProfit(vector<int>& prices) {
+        vector<vector<int>> dp(prices.size(),vector<int>(2,-1));
+        return fn(0,true,prices,dp);
+    }
+};
