e2de8681eaa1c811baf8ad38872b8c8788aa0a5e
Kamalesh-choudhary <157907909+Kamalesh-choudhary@users.noreply.github.com>
2026-08-23 23:46:30 +0530

    [123] [Time Beats: 71.22%] [Memory Beats: 74.88%] - LeetPush

--- Data Structures & Algorithms/123-best-time-to-buy-and-sell-stock-iii/123-Best-Time-to-Buy-and-Sell-Stock-III.cpp
+++ Data Structures & Algorithms/123-best-time-to-buy-and-sell-stock-iii/123-Best-Time-to-Buy-and-Sell-Stock-III.cpp
@@ -1,5 +1,7 @@
 class Solution {
 public:
+    //1. Recursive + Memo
+    /*
     int fn(int i,int buy,int cap,vector<int>& prices,vector<vector<vector<int>>>& dp){
         if(cap == 0){
             return 0;
@@ -23,5 +25,58 @@ public:
         vector<vector<vector<int>>> dp(n+1,vector<vector<int>>(2,vector<int>(3,-1)));
         return fn(0,1,2,prices,dp);
 
+    }
+    */
+
+    //2. Bottom UP DP
+    /*
+    int maxProfit(vector<int>& prices) {
+        int n = prices.size();
+        vector<vector<vector<int>>> dp(
+            n+1 , vector<vector<int>>(2, vector<int>(3, 0)));
+
+        for(int i=n-1;i>=0;i--){
+            for(int buy = 1;buy>=0;buy--){
+                for(int cap = 1;cap<=2;cap++){
+                    int profit = 0;
+                    if(buy){
+                        profit = max(-prices[i] + dp[i + 1][0][cap],
+                         0 + dp[i + 1][1][cap]);
+                    }
+                    else{
+                        profit = max(prices[i]+dp[i+1][1][cap-1],0+dp[i+1][0][cap]);
+                    }
+                    dp[i][buy][cap] = profit;
+                }
+            }
+        }
+
+        return dp[0][1][2];
+    }
+    */
+
+    //3. Space Optimization
+    int maxProfit(vector<int>& prices) {
+        int n = prices.size();
+        vector<vector<int>> front(2,vector<int>(3,0)),curr(2,vector<int>(3,0));
+
+        for (int i = n - 1; i >= 0; i--) {
+            for (int buy = 1; buy >= 0; buy--) {
+                for (int cap = 1; cap <= 2; cap++) {
+                    int profit = 0;
+                    if (buy) {
+                        profit = max(-prices[i] + front[0][cap],
+                                     0 + front[1][cap]);
+                    } else {
+                        profit = max(prices[i] + front[1][cap - 1],
+                                     0 + front[0][cap]);
+                    }
+                    curr[buy][cap] = profit;
+                }
+            }
+            front = curr;
+        }
+
+        return front[1][2];
     }
 };
