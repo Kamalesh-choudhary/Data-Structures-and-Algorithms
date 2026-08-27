7e4c49f2aa7799c0974c3d08f338ab9c7c68328d
Kamalesh-choudhary <157907909+Kamalesh-choudhary@users.noreply.github.com>
2026-08-28 00:15:07 +0530

    [1547] [Time Beats: 84.72%] [Memory Beats: 81.20%] - LeetPush

--- Data Structures & Algorithms/1547-minimum-cost-to-cut-a-stick/1547-Minimum-Cost-to-Cut-a-Stick.cpp
+++ Data Structures & Algorithms/1547-minimum-cost-to-cut-a-stick/1547-Minimum-Cost-to-Cut-a-Stick.cpp
@@ -1,5 +1,8 @@
 class Solution {
 public:
+/*
+    //[1]. Recursion+ Memo =>
+
     int fn(int i,int j,vector<int>& ar,vector<vector<int>>& dp){
         if(i>j) return 0;
         if(dp[i][j] != -1) return dp[i][j];
@@ -16,5 +19,28 @@ public:
         sort(cuts.begin(),cuts.end());
         vector<vector<int>> dp(cuts.size(),vector<int>(cuts.size(),-1));
         return fn(1,cuts.size()-2,cuts,dp);
+    }
+*/
+    //[2]. Bottom UP Dynamic Programming =>
+
+    int minCost(int n, vector<int>& cuts) {
+        cuts.push_back(n);
+        cuts.insert(cuts.begin(),0);
+        sort(cuts.begin(),cuts.end());
+        n = cuts.size();
+        vector<vector<int>> dp(n,vector<int>(n,0));
+
+        for(int i=n-1;i>=1;i--){
+            for(int j=1;j<n-1;j++){
+                if(i>j) continue;
+                int ans = INT_MAX;
+                for(int k=i;k<=j;k++){
+                    int cost = cuts[j+1] - cuts[i-1] + dp[i][k-1] + dp[k+1][j];
+                    ans = min(ans,cost);
+                }
+                dp[i][j] = ans;
+            }
+        }
+        return dp[1][n-2];
     }
 };
