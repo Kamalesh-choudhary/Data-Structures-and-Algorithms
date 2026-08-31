fa241bdf8cc27310b2161312733889c1c2d2dad0
Kamalesh-choudhary <157907909+Kamalesh-choudhary@users.noreply.github.com>
2026-09-01 02:00:53 +0530

    Add Fence [B] from Codeforces

---
+++ 363/B - Fence/solution.cpp
@@ -0,0 +1,70 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+// --- Type Aliases ---
+using ll  = long long;
+using ull = unsigned long long;
+using ld  = long double;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using vi  = vector<int>;
+using vll = vector<ll>;
+using vvi = vector<vector<int>>;
+using vvll= vector<vector<ll>>;
+using si  = set<int>;
+using mi  = map<int, int>;
+ 
+// --- Constants ---
+const int MOD  = 1e9 + 7;
+const int INF  = 1e9;
+const ll  LINF = 1e18;
+const ld  PI   = acos((ld)-1);
+ 
+// --- Macros ---
+#define all(x)       (x).begin(), (x).end()
+#define rall(x)      (x).rbegin(), (x).rend()
+#define pb           push_back
+#define mp           make_pair
+#define fi           first
+#define se           second
+#define sz(x)        (int)(x).size()
+#define rep(i, a, b) for (int i = (a); i < (b); i++)
+#define per(i, a, b) for (int i = (b)-1; i >= (a); i--)
+ 
+// --- Debug ---
+#ifdef DEBUG
+#define dbg(x) cerr << #x << " = " << x << "
+"
+#else
+#define dbg(x)
+#endif
+ 
+ 
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    int n,k;
+    cin >> n >> k;
+    vector<int> ar(n);
+    for(auto &x:ar) cin >> x;
+ 
+    int window_sum = 0;
+    int index = 0;
+    for(int i=0;i<k;i++){
+        window_sum+= ar[i];
+    }
+    int min_window_sum = window_sum;
+    int l = 0;
+    for(int i=k;i<n;i++){
+        window_sum += ar[i];
+        window_sum -= ar[l];
+        l++;
+        if(window_sum < min_window_sum){
+            min_window_sum = window_sum;
+            index = i-k+1;
+        }
+    }
+ 
+    cout << index+1 << endl;
+}
