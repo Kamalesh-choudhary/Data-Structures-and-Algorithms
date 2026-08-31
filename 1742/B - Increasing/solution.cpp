999667f018ec01bc73167dee2a0ab5378b17f163
Kamalesh-choudhary <157907909+Kamalesh-choudhary@users.noreply.github.com>
2026-09-01 01:54:57 +0530

    Add Increasing [B] from Codeforces

---
+++ 1742/B - Increasing/solution.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main(){
+  int t;
+  cin >> t;
+  while(t--){
+    int n;
+    cin >> n;
+    vector<int> vec(n);
+    for (auto &x:vec) cin >> x;
+    set<int> check;
+    for (int i=0;i<n;i++){
+      check.insert(vec[i]);
+    }
+    if (check.size() == vec.size()){
+      cout << "YES" << endl;
+    }
+    else{
+      cout << "NO" << endl;
+    }
+  }
+}
