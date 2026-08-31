2babf5a4e8413b8ce7bbcdde7a63d67290cb4a15
Kamalesh-choudhary <157907909+Kamalesh-choudhary@users.noreply.github.com>
2026-09-01 02:01:21 +0530

    Add Ilya and Queries [B] from Codeforces

---
+++ 313/B - Ilya and Queries/solution.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+// --- Type Aliases ---
+using ll  = long long;
+using ull = unsigned long long;
+using ld  = long double;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using vi  = vector<int>;
+using vll = vector<ll>;
+using vvi = vector<vector<int>>;
+using vvll= vector<vector<ll>>;
+using si  = set<int>;
+using mi  = map<int, int>;
+ 
+// --- Constants ---
+const int MOD  = 1e9 + 7;
+const int INF  = 1e9;
+const ll  LINF = 1e18;
+const ld  PI   = acos((ld)-1);
+ 
+// --- Macros ---
+#define all(x)       (x).begin(), (x).end()
+#define rall(x)      (x).rbegin(), (x).rend()
+#define pb           push_back
+#define mp           make_pair
+#define fi           first
+#define se           second
+#define sz(x)        (int)(x).size()
+#define rep(i, a, b) for (int i = (a); i < (b); i++)
+#define per(i, a, b) for (int i = (b)-1; i >= (a); i--)
+ 
+// --- Debug ---
+#ifdef DEBUG
+#define dbg(x) cerr << #x << " = " << x << "
+"
+#else
+#define dbg(x)
+#endif
+ 
+ 
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    string s;
+    cin >> s;
+ 
+    int m;
+    cin >> m;
+    vector<int> prefix(s.size(), 0);
+    for(int i = 1; i < s.size(); i++) {
+        prefix[i] = prefix[i-1] + (s[i] == s[i-1]);
+    }
+    rep(i,0,m){
+        int l,r;
+        cin >> l >> r;
+        l--;
+        r--;
+        cout << prefix[r]-prefix[l] << endl;
+    }
+}
