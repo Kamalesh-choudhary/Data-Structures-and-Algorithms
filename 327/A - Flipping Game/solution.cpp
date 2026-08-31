74da9b6703966f712d6d52d48e45883266cd4cb8
Kamalesh-choudhary <157907909+Kamalesh-choudhary@users.noreply.github.com>
2026-09-01 02:02:01 +0530

    Add Flipping Game [A] from Codeforces

---
+++ 327/A - Flipping Game/solution.cpp
@@ -0,0 +1,69 @@
+#include <bits/stdc++.h>
+#include <climits>
+using namespace std;
+ 
+// --- Type Aliases ---
+using ll  = long long;
+using ull = unsigned long long;
+using ld  = long double;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using vi  = vector<int>;
+using vll = vector<ll>;
+using vvi = vector<vector<int>>;
+using vvll= vector<vector<ll>>;
+using si  = set<int>;
+using mi  = map<int, int>;
+ 
+// --- Constants ---
+const int MOD  = 1e9 + 7;
+const int INF  = 1e9;
+const ll  LINF = 1e18;
+const ld  PI   = acos((ld)-1);
+ 
+// --- Macros ---
+#define all(x)       (x).begin(), (x).end()
+#define rall(x)      (x).rbegin(), (x).rend()
+#define pb           push_back
+#define mp           make_pair
+#define fi           first
+#define se           second
+#define sz(x)        (int)(x).size()
+#define rep(i, a, b) for (int i = (a); i < (b); i++)
+#define per(i, a, b) for (int i = (b)-1; i >= (a); i--)
+ 
+// --- Debug ---
+#ifdef DEBUG
+#define dbg(x) cerr << #x << " = " << x << "
+"
+#else
+#define dbg(x)
+#endif
+ 
+ 
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    int n;
+    cin >> n;
+    vector<int> ar(n);
+    for(auto &x:ar) cin >> x;
+ 
+    int best = INT_MIN;
+    rep(i,0,n){
+        int curr = 0;
+        rep(j,i,n){
+            curr += (ar[j]==0)?1:-1;
+            best = max(best,curr);
+        }
+    }
+ 
+    int ones = count(ar.begin(),ar.end(),1);
+    if(ones == n){
+        cout << n-1 << endl;
+    }
+    else{
+        cout << ones+best << endl;
+    }
+}
