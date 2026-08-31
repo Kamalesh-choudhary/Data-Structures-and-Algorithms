df4efda60cb8ddf39ebbc37ce3f929be1e36e1ae
Kamalesh-choudhary <157907909+Kamalesh-choudhary@users.noreply.github.com>
2026-09-01 02:01:52 +0530

    Add Given Length and Sum of Digits... [C] from Codeforces

---
+++ 489/C - Given Length and Sum of Digits.../solution.cpp
@@ -0,0 +1,81 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+// --- Type Aliases ---
+using ll  = long long;
+using ull = unsigned long long;
+using ld  = long double;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using vi  = vector<int>;
+using vll = vector<ll>;
+using vvi = vector<vector<int>>;
+using vvll= vector<vector<ll>>;
+using si  = set<int>;
+using mi  = map<int, int>;
+ 
+// --- Constants ---
+const int MOD  = 1e9 + 7;
+const int INF  = 1e9;
+const ll  LINF = 1e18;
+const ld  PI   = acos((ld)-1);
+ 
+// --- Macros ---
+#define all(x)       (x).begin(), (x).end()
+#define rall(x)      (x).rbegin(), (x).rend()
+#define pb           push_back
+#define mp           make_pair
+#define fi           first
+#define se           second
+#define sz(x)        (int)(x).size()
+#define rep(i, a, b) for (int i = (a); i < (b); i++)
+#define per(i, a, b) for (int i = (b)-1; i >= (a); i--)
+ 
+// --- Debug ---
+#ifdef DEBUG
+#define dbg(x) cerr << #x << " = " << x << "
+"
+#else
+#define dbg(x)
+#endif
+ 
+ 
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+ 
+    int m,s;
+    cin >> m >> s;
+ 
+    if (9*m < s || (s==0 && m>1)){
+        cout << -1 << " " << -1 << "
+";
+        return 0;
+    }
+    if(s==0 && m==1){
+        cout << 0 << " "<< 0 << endl;
+        return 0;
+    }
+ 
+    vi dmax(m,0);
+    int rem = s;
+    rep(i,0,m){
+        dmax[i] = min(9,rem);
+        rem -= dmax[i];
+    }
+ 
+    vi dmin(m,0);
+    rem = s;
+    dmin[0] = max(1,rem-9*(m-1));
+    rem -= dmin[0];
+    per(i,1,m){
+        dmin[i] = min(9,rem);
+        rem -= dmin[i];
+    }
+ 
+    string maxi,mini;
+    rep(i,0,m) maxi += char('0'+dmax[i]);
+    rep(i,0,m) mini += char('0'+dmin[i]);
+ 
+    cout << mini << " " << maxi << endl;
+}
