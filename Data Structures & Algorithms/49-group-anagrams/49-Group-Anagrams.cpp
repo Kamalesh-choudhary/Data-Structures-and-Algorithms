2ec1d4034e48c3b62ce056337aae45848168c485
Kamalesh-choudhary <157907909+Kamalesh-choudhary@users.noreply.github.com>
2026-09-03 13:52:01 +0530

    [49] [Time Beats: 69.52%] [Memory Beats: 31.46%] - LeetPush

---
+++ Data Structures & Algorithms/49-group-anagrams/49-Group-Anagrams.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        if(strs.empty()) return {{}};
+        unordered_map<string,vector<string>> mp;
+        int n = strs.size();
+        for(int i=0;i<n;i++){
+            string temp = strs[i];
+            sort(temp.begin(),temp.end());
+            mp[temp].push_back(strs[i]);
+        }
+        vector<vector<string>> ans;
+        for(auto [key,val]:mp){
+            ans.push_back(val);
+        }
+        return ans;
+    }
+};
