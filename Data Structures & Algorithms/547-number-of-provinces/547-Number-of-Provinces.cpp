7989412a3d284747ccc35d834229d5dcfee72fe2
Kamalesh-choudhary <157907909+Kamalesh-choudhary@users.noreply.github.com>
2026-09-08 14:46:47 +0530

    [547] [Time Beats: 100.00%] [Memory Beats: 82.10%] - LeetPush

--- Data Structures & Algorithms/547-number-of-provinces/547-Number-of-Provinces.cpp
+++ Data Structures & Algorithms/547-number-of-provinces/547-Number-of-Provinces.cpp
@@ -1,5 +1,7 @@
 class Solution {
 public:
+    // BFS Solution
+    /*
     int findCircleNum(vector<vector<int>>& isConnected) {
         int n = isConnected.size();
         vector<bool> vis(n+1,false);
@@ -23,6 +25,29 @@ public:
             }
         }
 
+        return count;
+    }
+    */
+
+    // DFS Solution
+    void dfs(int node,vector<vector<int>>& matrix,vector<bool>& vis){
+        vis[node] = true;
+        for(int i=0;i<matrix[node-1].size();i++){
+            if(matrix[node-1][i] == 1 && !vis[i+1]){
+                dfs(i+1,matrix,vis);
+            }
+        }
+    }
+    int findCircleNum(vector<vector<int>>& isConnected) {
+        int n = isConnected.size();
+        vector<bool> vis(n+1,false);
+        int count = 0;
+        for(int i=1;i<=n;i++){
+            if(!vis[i]){
+                count++;
+                dfs(i,isConnected,vis);
+            }
+        }
         return count;
     }
 };
