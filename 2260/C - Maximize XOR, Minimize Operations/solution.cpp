65c5f307fee453f615b62f37ece00468e5a9d6f0
Kamalesh-choudhary <157907909+Kamalesh-choudhary@users.noreply.github.com>
2026-09-08 21:00:55 +0530

    Add Maximize XOR, Minimize Operations [C] from Codeforces

---
+++ 2260/C - Maximize XOR, Minimize Operations/solution.cpp
@@ -0,0 +1,64 @@
+/*
+ * Author : Kamalesh Choudhary
+ * Date   : 08-09-2026
+ */
+ 
+#include <bits/stdc++.h>
+using namespace std;
+ 
+using i32 = int;
+using i64 = long long;
+using u32 = unsigned int;
+using u64 = unsigned long long;
+ 
+using pii = pair<i32, i32>;
+using pll = pair<i64, i64>;
+ 
+using vi = vector<i32>;
+using vll = vector<i64>;
+ 
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+ 
+const i32 MOD = 1'000'000'007;
+const i32 INF = 1'000'000'000;
+const i64 LINF = 4'000'000'000'000'000'000LL;
+ 
+void solve() {
+  int x, y;
+  cin >> x >> y;
+ 
+  int s = x + y;
+  int a = 0;
+  bool smaller = false;
+ 
+  for (int i = 30; i >= 0; i--) {
+    if (smaller) {
+      if (s & (1 << i))
+        a |= (1 << i);
+    } else {
+      if (x & (1 << i)) {
+        if (s & (1 << i))
+          a |= (1 << i);
+        else {
+          smaller = true;
+        }
+      }
+    }
+  }
+  cout << s << " " << x - a << endl;
+}
+ 
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+ 
+  i32 t;
+  cin >> t;
+ 
+  while (t--) {
+    solve();
+  }
+ 
+  return 0;
+}
