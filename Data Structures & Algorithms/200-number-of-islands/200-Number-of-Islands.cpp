1bd29d370652d1650a7af4f27372e26ce8266e99
Kamalesh-choudhary <157907909+Kamalesh-choudhary@users.noreply.github.com>
2026-09-08 15:18:35 +0530

    [200] [Time Beats: 91.76%] [Memory Beats: 9.08%] - LeetPush

--- Data Structures & Algorithms/200-number-of-islands/200-Number-of-Islands.cpp
+++ Data Structures & Algorithms/200-number-of-islands/200-Number-of-Islands.cpp
@@ -1,5 +1,7 @@
 class Solution {
 public:
+    //1. DFS Solution
+    /*
     void dfs(int i,int j,vector<vector<char>>& grid){
         grid[i][j] = '0';
         vector<pair<int,int>> dir = {
@@ -32,6 +34,52 @@ public:
                 }
             }
         }
+        return cnt;
+    }
+    */
+
+    // BFS Solution
+    void bfs(int i,int j,vector<vector<char>>& grid,vector<vector<int>>& vis){
+        grid[i][j] = '0';
+        vector<pair<int,int>> dir = {
+            {0,1},
+            {1,0},
+            {-1,0},
+            {0,-1}
+        };
+        queue<pair<int,int>> q;
+        q.push({i,j});
+        vis[i][j] = 1;
+
+        while(!q.empty()){
+            auto [r,c] = q.front();
+            q.pop();
+
+            for(auto [x,y]:dir){
+                int ni = r+x;
+                int nj = c+y;
+                if(ni>=0 && ni<grid.size() && nj>=0 && nj<grid[0].size() && grid[ni][nj] =='1' && !vis[ni][nj]){
+                    vis[ni][nj] = 1;
+                    grid[ni][nj] = '0';
+                    q.push({ni,nj});
+                }
+            }        
+        }
+    }
+    int numIslands(vector<vector<char>>& grid) {
+        int cnt = 0;
+        int m = grid.size();
+        int n = grid[0].size();
+        vector<vector<int>> vis(m,vector<int>(n,0));
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(grid[i][j] == '1'){
+                    cnt++;
+                    bfs(i,j,grid,vis);
+                }
+            }
+        }
+
         return cnt;
     }
 };
