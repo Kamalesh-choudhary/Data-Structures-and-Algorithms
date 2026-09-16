0f3a42aed788883f2d9ced54902900b798ca9677
Kamalesh-choudhary <157907909+Kamalesh-choudhary@users.noreply.github.com>
2026-09-16 20:14:01 +0530

    Add Left and Down [B] from Codeforces

---
+++ 2125/B - Left and Down/solution.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+using i32 = int;
+using i64 = long long;
+using u32 = unsigned int;
+using u64 = unsigned long long;
+ 
+using pii = pair<i32, i32>;
+using pll = pair<i64, i64>;
+ 
+using vi = vector<i32>;
+using vll = vector<i64>;
+ 
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+ 
+const i32 MOD = 1'000'000'007;
+const i32 INF = 1'000'000'000;
+const i64 LINF = 4'000'000'000'000'000'000LL;
+ 
+class Solution {
+public:
+  void solve() {
+    i64 x, y, k;
+    cin >> x >> y >> k;
+ 
+    i64 g = gcd(x, y);
+    i64 dx = x / g;
+    i64 dy = y / g;
+    i64 ans = max(dx, dy);
+    if (ans <= k)
+      cout << 1 << endl;
+    else
+      cout << 2 << endl;
+  }
+};
+ 
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+ 
+  Solution sol;
+ 
+  i32 t;
+  cin >> t;
+ 
+  while (t--) {
+    sol.solve();
+  }
+ 
+  return 0;
+}
