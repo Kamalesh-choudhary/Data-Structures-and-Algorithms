2c0000a47f48fc1fce8841cd664b4e819df214fe
Kamalesh-choudhary <157907909+Kamalesh-choudhary@users.noreply.github.com>
2026-09-22 10:30:30 +0530

    Add Theatre Square [A] from Codeforces

--- 1/A - Theatre Square/solution.cpp
+++ 1/A - Theatre Square/solution.cpp
@@ -2,13 +2,13 @@
 using namespace std;
 using ll = long long;
  
-int main(){
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-    
-    ll n,m,a;
-    cin >> n >> m >> a;
-    
-    ll ans = ((n+a-1)/a) * ((m+a-1)/a);
-    cout << ans << endl;
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+ 
+  ll n, m, a;
+  cin >> n >> m >> a;
+ 
+  ll ans = ((n + a - 1) / a) * ((m + a - 1) / a);
+  cout << ans << endl;
 }
