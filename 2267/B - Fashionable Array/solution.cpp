eecdd44dd9d9ed9c671a4dbb5b23e2288689ecd0
Kamalesh-choudhary <157907909+Kamalesh-choudhary@users.noreply.github.com>
2026-09-25 20:55:55 +0530

    Add Fashionable Array [B] from Codeforces

---
+++ 2267/B - Fashionable Array/solution.cpp
@@ -0,0 +1,73 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+using i32 = int;
+using i64 = long long;
+using u32 = unsigned int;
+using u64 = unsigned long long;
+ 
+using pii = pair<i32, i32>;
+using pll = pair<i64, i64>;
+ 
+using vi = vector<i32>;
+using vll = vector<i64>;
+ 
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+ 
+const i32 MOD = 1'000'000'007;
+const i32 INF = 1'000'000'000;
+const i64 LINF = 4'000'000'000'000'000'000LL;
+ 
+class Solution {
+public:
+  void solve() {
+    i32 n;
+    cin >> n;
+ 
+    vi ar(n);
+    for (auto &x : ar)
+      cin >> x;
+ 
+    map<i32, i32, greater<i32>> mp;
+ 
+    for (auto x : ar)
+      mp[x]++;
+ 
+    i32 maxi = 0;
+ 
+    for (auto [key, val] : mp)
+      maxi = max(maxi, val);
+ 
+    vi ans;
+ 
+    for (i32 level = 1; level <= maxi; level++) {
+      for (auto &[key, val] : mp) {
+        if (val >= level)
+          ans.push_back(key);
+      }
+    }
+ 
+    for (auto x : ans)
+      cout << x << " ";
+ 
+    cout << '
+';
+  }
+};
+ 
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+ 
+  Solution sol;
+ 
+  i32 t;
+  cin >> t;
+ 
+  while (t--) {
+    sol.solve();
+  }
+ 
+  return 0;
+}
